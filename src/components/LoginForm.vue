d51677d736551262013d0dac6f51b9c15061c98b
Vescik <wescik992@gmail.com>
2023-06-19 18:40:48 +0200

    created login and register components

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import useLogin from '@/composable/useLogin'
+import { ref } from 'vue';
+const username = ref('')
+const password = ref('')
+
+const handleSubmit = async () => {
+      await login(username.value, password.value)
+    }
+
+const { login,error } = useLogin()
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input v-model="username" type="text" id="username" name="username" />
+    <input v-model="password" type="password" id="password" name="password" />
+    <div>{{ error }}</div>
+    <button  type="submit">Login</button>
+  </form>
+</template>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,27 @@
+<template>
+     <form @submit.prevent="handleSubmit">
+    <input v-model="username" type="text" name="username" />
+    <input v-model="email" type="text" name="email" />
+    <input v-model="password" type="password" id="password" name="password" />
+    <div>{{ error }}</div>
+    <button  type="submit">Login</button>
+  </form>
+</template>
+
+<script setup lang="ts">
+ import { ref } from 'vue';
+ import useSignup from '@/composable/useSignup'
+    const username = ref('')
+    const email = ref('')
+    const password = ref('')
+    const { signup,error } = useSignup()
+
+    const handleSubmit = async () => {
+      await signup(email.value, password.value)
+    }
+
+</script>
+
+<style lang="ts" scoped>
+
+</style>
